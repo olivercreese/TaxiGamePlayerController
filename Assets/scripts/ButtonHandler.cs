20fc0888a0ef0f111b6b79ecf7c755891dfa7861
bjat39 <bjat39@googlemail.com>
2024-11-23 18:02:55 +0000

    very basic main menu

--- Assets/scripts/ButtonHandler.cs
+++ Assets/scripts/ButtonHandler.cs
@@ -1,16 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour
+public class ButtonHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void onclickButton1()
     {
-        
+        Debug.Log("button1");
+        GoToNextScene();
+    }
+    public void onclickButton2()
+    {
+        Debug.Log("button2");
     }
 
-    // Update is called once per frame
-    void Update()
+    void GoToNextScene()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Check to prevent going out of bounds
+        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            Debug.Log("This is the last scene.");
+            // Optionally, you can load the first scene or do something else.
+            // SceneManager.LoadScene(0); // Load first scene
+        }
     }
 }
+
