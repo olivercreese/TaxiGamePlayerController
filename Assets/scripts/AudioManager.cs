e9dff6878f8d9a2a4c7cb551e1353cc9a649a49c
bjat39 <bjat39@googlemail.com>
2024-11-24 17:29:11 +0000

    unity

--- Assets/scripts/AudioManager.cs
+++ Assets/scripts/AudioManager.cs
@@ -5,4 +5,10 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource musicSource;
 
     public AudioClip background;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
 }
